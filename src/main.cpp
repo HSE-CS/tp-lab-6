b48a8c616a982d87e3cecbf2f476e0a8550f0a28
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-13 01:34:21 +0300

    uncomment, add conversion to char pointer from std::string

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 int main() {
   // создание штата сотрудников на основе файла
-  std::vector<Employee *> staff; // = Factory::makeStaff(
-//      "/home/stanislav/CLionProjects/tp-lab-6/test/data/employes.csv");
+  std::string string = "/home/stanislav/CLionProjects/tp-lab-6/test/data/employees.csv";
+  std::vector<Employee *> staff = Factory::makeStaff(&string[0]);
 
   // присвоение отработанного времени и расчет зарплаты, вывод данных о зарплате
   for (Employee *employee : staff) {
