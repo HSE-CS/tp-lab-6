4c2679b06cda59607a378027bbb360e95e69f51f
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-13 18:02:21 +0300

    add more tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,7 +9,6 @@ TEST(Employee, Constructor) {
   auto *employee =
       new Personal(1, "Ivan", "SEO of Feminism", 2);
   EXPECT_EQ(employee->getPayment(), 2);
-  EXPECT_EQ(employee->getSalary(), 0);
 }
 
 TEST(Driver, Constructor) {
@@ -34,12 +33,30 @@ TEST(Engineer, Constructor) {
   EXPECT_EQ(employee->getSalary(), 0);
 }
 
+TEST(Engineer, Salary) {
+  auto *project = new Project(1, 200);
+  auto *employee =
+      new Engineer(1, "Ivan", "SEO of Feminism", 2, project);
+  employee->setWorktime(40);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_EQ(employee->getSalary(), 80);
+}
+
 TEST(Programmer, Constructor) {
   auto *project = new Project(1, 200);
   auto *employee =
       new Programmer(1, "Ivan", "SEO of Feminism", 2, project);
   EXPECT_EQ(employee->getPayment(), 2);
   EXPECT_EQ(employee->getSalary(), 0);
+}
+
+TEST(Programmer, Salary) {
+  auto *project = new Project(1, 200);
+  auto *employee =
+      new Programmer(1, "Ivan", "SEO of Feminism", 2, project);
+  employee->setWorktime(40);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_EQ(employee->getSalary(), 80);
 }
 
 TEST(Tester, Constructor) {
@@ -50,12 +67,30 @@ TEST(Tester, Constructor) {
   EXPECT_EQ(employee->getSalary(), 0);
 }
 
+TEST(Tester, Salary) {
+  auto *project = new Project(1, 200);
+  auto *employee =
+      new Tester(1, "Ivan", "SEO of Feminism", 2, project);
+  employee->setWorktime(40);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_NE(employee->getSalary(), 90);
+}
+
 TEST(TeamLeader, Constructor) {
   auto *project = new Project(1, 200);
   auto *employee =
       new TeamLeader(1, "Ivan", "SEO of Feminism", 2, project);
   EXPECT_EQ(employee->getPayment(), 2);
-  EXPECT_EQ(employee->getSalary(), 0);
+  EXPECT_EQ(employee->getSalary(), 2);
+}
+
+TEST(TeamLeader, Salary) {
+  auto *project = new Project(1, 200);
+  auto *employee =
+      new TeamLeader(1, "Ivan", "SEO of Feminism", 2, project);
+  employee->setWorktime(40);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_EQ(employee->getSalary(), 82);
 }
 
 TEST(ProjectManager, Constructor) {
@@ -63,6 +98,15 @@ TEST(ProjectManager, Constructor) {
   auto *employee =
       new ProjectManager(1, "Ivan", "SEO of Feminism", 2, project);
   EXPECT_EQ(employee->getPayment(), 2);
+}
+
+TEST(ProjectManager, Salary) {
+  auto *project = new Project(1, 200);
+  auto *employee =
+      new ProjectManager(1, "Ivan", "SEO of Feminism", 2, project);
+  employee->setWorktime(40);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_EQ(employee->getSalary(), 82);
 }
 
 TEST(SeniorManager, Constructor) {
@@ -73,6 +117,19 @@ TEST(SeniorManager, Constructor) {
   projects.push_back(project2);
   auto *employee = new SeniorManager(
       1, "Ivan", "SEO of Feminism", 2, project, projects);
+  EXPECT_EQ(employee->getPayment(), 2);
+  EXPECT_EQ(employee->getProjects().size(), 2);
+}
+
+TEST(SeniorManager, Salary) {
+  auto *project = new Project(1, 400);
+  auto *project2 = new Project(2, 400);
+  std::vector<Project *> projects;
+  projects.push_back(project);
+  projects.push_back(project2);
+  auto *employee = new SeniorManager(
+      1, "Ivan", "SEO of Feminism", 2, project, projects);
+  employee->setWorktime(40);
   EXPECT_EQ(employee->getPayment(), 2);
   EXPECT_EQ(employee->getProjects().size(), 2);
 }
