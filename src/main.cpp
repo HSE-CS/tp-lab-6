bd6eb5f3ba57c17685c33965f4e67b1eed220830
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-13 16:35:00 +0300

    reformat example

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
 // Copyright 2020 Stanislav Stoianov
 
 #include "../include/Factory.h"
+#include <random>
+
+static std::mt19937_64 rng;
 
 int main() {
-  // создание штата сотрудников на основе файла
+  /**
+   * создание штата сотрудников на основе файла
+   */
   std::string string =
       "/home/stanislav/CLionProjects/tp-lab-6/test/data/employees.csv";
-  std::vector<Employee *> staff = Factory::makeStaff(&string[0]);
+  std::string string2 =
+      "/home/stanislav/CLionProjects/tp-lab-6/test/data/projects.csv";
+  std::vector<Employee *> staff =
+      Factory::makeStaff(&string[0], &string2[0]);
 
-  // присвоение отработанного времени и расчет зарплаты, вывод данных о зарплате
+  /**
+   * присвоение отработанного времени
+   * расчет зарплаты
+   * вывод данных о зарплате
+   */
   for (Employee *employee : staff) {
-    employee->setWorktime(0);
+    employee->setWorktime(static_cast<int>(rng() % 20 + 40));
     employee->calc();
     employee->printInfo();
   }
