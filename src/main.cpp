5ac5812a276221ee8986361b56acdcb7a3da79e6
Arina Simonova <kotyonok2001@mail.ru>
2021-02-20 16:36:02 +0300

    changed main for cpplint. Sorry for using namespace :(

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,10 @@ int main() {
   string proj = "G:\\GitHubTasks\\tp-lab-6\\projects.txt";
   string myStaff = "G:\\GitHubTasks\\tp-lab-6\\MyStaff.txt";
   srand(time(NULL));
- vector<Employee *> staff = makeStaff(&myStaff[0], &proj[0]);
+  vector<Employee *> staff = makeStaff(&myStaff[0], &proj[0]);
   for (int i = 0; i < 50; ++i) {
-    staff[i]->SetWorkTime(20 + rand() % 35);
+    staff[i]->SetWorkTime(20 + rand_r() % 35);
     staff[i]->Calc();
     staff[i]->PrintInfo();
   }
-
 }
