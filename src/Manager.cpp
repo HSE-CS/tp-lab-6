376417f973eafd38666959d63986dbb0430b2498
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:34:22 +0300

    Update Manager.cpp

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,53 +1,54 @@
 #include "Manager.h"
+#include <string>
 
 ProjectManager::ProjectManager(unsigned int id, std::string name, Position position, std::string project) :
-	Employee(id, name, position) {
-	this->project.id = project;
-	this->project.budget = 300000;
+    Employee(id, name, position) {
+    this->project.id = project;
+    this->project.budget = 300000;
 }
 
-unsigned int ProjectManager::calcHeads() {
-	return 200;
+int ProjectManager::calcHeads() {
+    return 200;
 }
 
 unsigned int ProjectManager::calcProAdditions() {
-	return 5000;
+    return 5000;
 }
 
 std::string PositionStriing[] = {
-	"Project Manager", "Senior Manager"
+    "Project Manager", "Senior Manager"
 };
 
 void ProjectManager::printInfo() {
-	std::cout << "id " << this->Employee::id << std::endl;
-	std::cout << "name " << this->name << std::endl;
-	std::cout << "position " << PositionStriing[int(this->position) - 2] << std::endl;
-	std::cout << "work time " << this->worktime << std::endl;
-	std::cout << "payment " << this->payment << std::endl;
-	std::cout << "project " << this->project.id << std::endl;
-	std::cout << std::endl;
+    std::cout << "id " << this->Employee::id << std::endl;
+    std::cout << "name " << this->name << std::endl;
+    std::cout << "position " << PositionStriing[int(this->position) - 2] << std::endl;
+    std::cout << "work time " << this->worktime << std::endl;
+    std::cout << "payment " << this->payment << std::endl;
+    std::cout << "project " << this->project.id << std::endl;
+    std::cout << std::endl;
 }
 
-unsigned int ProjectManager::calcBudgetPart(double part, unsigned int budget) {
-	return unsigned int(part * budget);
+int ProjectManager::calcBudgetPart(double part, unsigned int budget) {
+    return int(part * budget);
 }
 
 void ProjectManager::calc() {
-	this->payment = this->worktime * calcHeads() + calcProAdditions() + 
-		calcBudgetPart(0.2, this->project.budget);
+    this->payment = this->worktime * calcHeads() + calcProAdditions() + 
+        calcBudgetPart(0.2, this->project.budget);
 }
 
 SeniorManager::SeniorManager(unsigned int id, std::string name, Position position, std::string project) :
-	ProjectManager(id, name, position, project) {
-	this->project.id = project;
-	this->project.budget = 200000;
+    ProjectManager(id, name, position, project) {
+    this->project.id = project;
+    this->project.budget = 200000;
 }
 
 void SeniorManager::calc() {
-	int t = this->worktime * calcBudgetPart(0.005, this->project.budget);
-	this->payment = t + calcProAdditions();
+    int t = this->worktime * calcBudgetPart(0.005, this->project.budget);
+    this->payment = t + calcProAdditions();
 }
 
 unsigned int SeniorManager::calcBudgetPart(double part, unsigned int budget) {
-	return unsigned int(part * budget);
+    return unsigned int(part * budget);
 }
