969903860ce6f10d49572f45f1446c644794b264
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:14:16 +0300

    Update Employee.h

--- include/Employee.h
+++ include/Employee.h
@@ -1,33 +1,35 @@
-#ifndef EMPLOYEE_H
+#ifndef INCLUDE_EMPLOYEE_H
 #define EMPLOYEE_H
 
 #include <string>
 
 enum class Position {
-	DRIVER, CLEANER, PROJECT_MANAGER, SENIOR_MANAGER, ENGINEER, TESTER, PROGRAMMER, TEAM_LEAD
+    DRIVER, CLEANER, PROJECT_MANAGER, SENIOR_MANAGER,
+	ENGINEER, TESTER, PROGRAMMER, TEAM_LEAD
 };
 
 class Employee {
 protected:
-	unsigned int id;
-	std::string name;
-	Position position;
-	unsigned int worktime;
-	unsigned int payment;
+    unsigned int id;
+    std::string name;
+    Position position;
+    unsigned int worktime;
+    unsigned int payment;
+
 public:
-	void setWorkTime(unsigned int wtime);
-	virtual void calc() = 0;
-	virtual void printInfo() = 0;
-	Employee(unsigned int id, std::string name,
-		Position position);
+    void setWorkTime(unsigned int wtime);
+    virtual void calc() = 0;
+    virtual void printInfo() = 0;
+    Employee(unsigned int id, std::string name,
+        Position position);
 
-	std::string getName();
+    std::string getName();
 
-	Position getPosition();
+    Position getPosition();
 
-	unsigned int getWorktime();
+    unsigned int getWorktime();
 
-	unsigned int getPayment();
+    unsigned int getPayment();
 };
 
-#endif
+#endif  // INCLUDE_EMPLOYEE_H_
