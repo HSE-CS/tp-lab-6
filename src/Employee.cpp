362a3298fa76a850eb98b44d042a3942cb6f1104
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 16:49:58 +0300

    Update Employee.cpp

--- src/Employee.cpp
+++ src/Employee.cpp
@@ -9,3 +9,19 @@ Employee::Employee(unsigned int id, std::string name, Position position) {
 void Employee::setWorkTime(unsigned int wtime) {
 	this->worktime = wtime;
 }
+
+
+std::string Employee::getName() {
+	return this->name;
+}
+
+Position Employee::getPosition() {
+	return this->position;
+}
+
+unsigned int Employee::getWorktime() {
+	return this->worktime;
+}
+unsigned int Employee::getPayment() {
+	return this->payment;
+}
