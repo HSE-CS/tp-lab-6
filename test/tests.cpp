8ce1b683ab59c8dc0a83022b3a387e0fe6cbc32b
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 22:07:00 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,37 +8,39 @@
 #include "Employee.h"
 
 TEST(test_cleaner, test1) {
-    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    Cleaner employee(1, "Igor", Position::CLEANER, 20);
     employee.setWorkTime(20);
     std::string name = employee.getName();
     EXPECT_EQ("Igor", name);
 }
 
 TEST(test_cleaner, test2) {
-    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    Cleaner employee(1, "Igor", Position::CLEANER, 20);
     employee.setWorkTime(20);
     int position = static_cast<int>(employee.getPosition());
     EXPECT_EQ(position, 1);
 }
 
 TEST(test_cleaner, test3) {
-    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    Cleaner employee(1, "Igor", Position::CLEANER, 20);
     employee.setWorkTime(20);
     int time =  employee.getWorktime();
     EXPECT_EQ(time, 20);
 }
 
 TEST(test_cleaner, test4) {
-    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    Cleaner employee(1, "Igor", Position::CLEANER, 20);
     employee.setWorkTime(20);
+    employee.calc();
     int payment =  employee.getPayment();
-    EXPECT_EQ(payment, 600);
+    EXPECT_EQ(payment, 400);
 }
 
 TEST(test_manager, test5) {
     ProjectManager employee
         (1, "Ivan", Position::PROJECT_MANAGER, "name of project");
     employee.setWorkTime(20);
+    employee.calc();
     int payment =  employee.getPayment();
     EXPECT_EQ(payment, 69000);
 }
@@ -91,6 +93,7 @@ TEST(test_lead, test11) {
 TEST(test_lead, test12) {
     TeamLead employee(1, "Max", Position::TEAM_LEAD, "name of project", 1200);
     employee.setWorkTime(10);
+    employee.calc();
     int payment =  employee.getPayment();
     EXPECT_EQ(payment, 137000);
 }
@@ -98,6 +101,7 @@ TEST(test_lead, test12) {
 TEST(test_enginer, test13) {
     Engineer employee(1, "Denis", Position::ENGINEER, "name of project", 800);
     employee.setWorkTime(10);
+    employee.calc();
     int payment =  employee.getPayment();
     EXPECT_EQ(payment, 18000);
 }
