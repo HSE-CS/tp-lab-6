c95257c6631bf2dafeab4f396933f5f924fd346c
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 21:41:55 +0300

    Update Factory.cpp

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -16,48 +16,31 @@ std::vector<Employee*> makeStaff() {
             getline(data_file, position, ';');
             getline(data_file, salary, ';');
             getline(data_file, project, '\n');
-            if (position == "driver")
-            {
+            if (position == "driver") {
                 employee.push_back(new Driver(stoi(id), name,
                     Position::DRIVER, stoi(salary)));
-            }
-            else if (position == "cleaner")
-            {
+            } else if (position == "cleaner") {
                 employee.push_back(new Cleaner(stoi(id), name,
                     Position::CLEANER, stoi(salary)));
-            }
-            else if (position == "project manager")
-            {
+            } else if (position == "project manager") {
                 employee.push_back(new ProjectManager(stoi(id),
                     name, Position::PROJECT_MANAGER, project));
-            }
-            else if (position == "senior manager")
-            {
+            } else if (position == "senior manager") {
                 employee.push_back(new SeniorManager(stoi(id),
                     name, Position::SENIOR_MANAGER, project));
-            }
-            else if (position == "engineer")
-            {
+            } else if (position == "engineer") {
                 employee.push_back(new Engineer(stoi(id), name,
                     Position::ENGINEER, project, stoi(salary)));
-            }
-            else if (position == "tester")
-            {
+            } else if (position == "tester") {
                 employee.push_back(new Tester(stoi(id), name,
                     Position::ENGINEER, project, stoi(salary)));
-            }
-            else if (position == "programmer")
-            {
+            } else if (position == "programmer") {
                 employee.push_back(new Programmer(stoi(id), name,
                     Position::PROGRAMMER, project, stoi(salary)));
-            }
-            else if (position == "team lead")
-            {
+            } else if (position == "team lead") {
                 employee.push_back(new TeamLead(stoi(id), name,
                     Position::TEAM_LEAD, project, stoi(salary)));
-            }
-            else
-            {
+            } else {
                 std::cout << "Something wrong!";
             }
         }
