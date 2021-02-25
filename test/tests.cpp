e0ebbc6dc805005048ec723c42981938180398ef
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 17:09:32 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,28 +9,85 @@
 
 TEST(test_cleaner, test1) {
   Employee* employee = new Cleaner(1,
-    "Igor", "driver", 20);
+    "Igor", Position::DRIVER, 20);
   std::string name = employee->getName();
   EXPECT_EQ("Igor", name);
 }
 
 TEST(test_cleaner, test2) {
   Employee* employee = new Cleaner(1,
-    "Igor", "driver", 20);
+    "Igor", Position::DRIVER, 20);
   int position = int(employee->getPosition());
   EXPECT_EQ(position, 1);
 }
 
 TEST(test_cleaner, test3) {
   Employee* employee = new Cleaner(1,
-    "Igor", "driver", 20);
+    "Igor", Position::DRIVER, 20);
   int time =  employee->getWorktime();
   EXPECT_EQ(time, 20);
 }
 
 TEST(test_cleaner, test4) {
   Employee* employee = new Cleaner(1,
-    "Igor", "driver", 20);
+    "Igor", Position::DRIVER, 20);
   int payment =  employee->getPayment();
   EXPECT_EQ(payment, 600);
 }
+
+TEST(test_cleaner, test5) {
+  Employee* employee = new ProjectManager(1,
+    "Ivan", Position::PROJECT_MANAGER, 20);
+  int payment =  employee->getPayment();
+  EXPECT_EQ(payment, 69000);
+}
+
+TEST(test_cleaner, test6) {
+  Employee* employee = new ProjectManager(1,
+    "Ivan", Position::PROJECT_MANAGER, 20);
+  std::string name  =  employee->getName();
+  EXPECT_EQ(name, "Ivan");
+}
+
+TEST(test_cleaner, test7) {
+  Employee* employee = new ProjectManager(1,
+    "Ivan", Position::PROJECT_MANAGER, 20);
+  int time =  employee->getWorktime();
+  EXPECT_EQ(time, 20);
+}
+
+TEST(test_cleaner, test7) {
+  Employee* employee = new ProjectManager(1,
+    "Ivan", Position::PROJECT_MANAGER, 20);
+  int position =  int(employee->getPosition());
+  EXPECT_EQ(position, 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
