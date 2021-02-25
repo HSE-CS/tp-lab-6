e00be29f213932193883bdee520f9ce4f1b01742
Arina Simonova <kotyonok2001@mail.ru>
2021-02-25 19:57:36 +0300

    rand_r() -> rand()

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@
 int main() {
   std::string proj = "projects.txt";
   std::string myStaff = "MyStaff.txt";
+  srand(time(NULL));
   std::vector<Employee *> staff = makeStaff(&myStaff[0], &proj[0]);
   for (int i = 0; i < 50; ++i) {
-    staff[i]->SetWorkTime(20 + rand_r(3)) % 35);
+    staff[i]->SetWorkTime(20 + rand() % 35);
     staff[i]->Calc();
     staff[i]->PrintInfo();
   }
