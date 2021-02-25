241378dab831beafca4d1ae76d2d2656013765af
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:28:57 +0300

    Update Engineer.h

--- include/Engineer.h
+++ include/Engineer.h
@@ -1,46 +1,48 @@
-#ifndef ENGINEER_H
-#define ENGINEER_H
+// Copyright 2021 by Fatin Maxim
+#ifndef INCLUDE_ENGINEER_H_
+#define INCLUDE_ENGINEER_H_
+
 #include "Interfaces.h"
 #include"Personal.h"
 
 class Engineer : public ProjectBudget, public Project, public Personal{
-protected:
-	Project project;
-public:
-	Engineer(unsigned int id, std::string name, Position position,
-		std::string project, unsigned int salary);
-	unsigned int calcBudgetPart(double part, unsigned int budget);
-	void calc();
-	void printInfo();
-	unsigned int calcProAdditions();
+ protected:
+    Project project;
+ public:
+    Engineer(unsigned int id, std::string name, Position position,
+        std::string project, unsigned int salary);
+    unsigned int calcBudgetPart(double part, unsigned int budget);
+    void calc();
+    void printInfo();
+    unsigned int calcProAdditions();
 };
 
 class Tester : public Engineer {
-public:
-	Tester(unsigned int id, std::string name, Position position,
-		std::string project, unsigned int salary);
-	void calc();
-	void printInfo();
-	unsigned int calcProAdditions();
+ public:
+    Tester(unsigned int id, std::string name, Position position,
+        std::string project, unsigned int salary);
+    void calc();
+    void printInfo();
+    unsigned int calcProAdditions();
 };
 
 class Programmer : public Engineer {
-public:
-	Programmer(unsigned int id, std::string name, Position position,
-		std::string project, unsigned int salary);
-	unsigned int calcProAdditions();
-	void calc();
-	void printInfo();
+ public:
+    Programmer(unsigned int id, std::string name, Position position,
+        std::string project, unsigned int salary);
+    unsigned int calcProAdditions();
+    void calc();
+    void printInfo();
 };
 
 class TeamLead : public Programmer {
-public:
-	TeamLead(unsigned int id, std::string name, Position position,
-		std::string project, unsigned int salary);
-	unsigned int calcProAdditions();
-	void calc();
-	void printInfo();
-	unsigned int  calcHeads();
+ public:
+    TeamLead(unsigned int id, std::string name, Position position,
+        std::string project, unsigned int salary);
+    unsigned int calcProAdditions();
+    void calc();
+    void printInfo();
+    unsigned int  calcHeads();
 };
 
-#endif
+#endif // INCLUDE_ENGINEER_H_
