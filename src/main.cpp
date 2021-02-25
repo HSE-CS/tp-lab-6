3f4102c3bfc2ab554d45676a1a7e0d445576f335
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 21:24:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
+// Copyright 2021 by Fatin Maxim
 #include <iostream>
 #include <string>
 #include <vector>
 #include "Factory.h"
 #include "Personal.h"
 #include "Interfaces.h"
-
 
 int main() {
     std::vector<Employee*> staff = makeStaff();
