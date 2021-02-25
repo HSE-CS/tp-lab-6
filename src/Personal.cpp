21b23093228df1b3a51c15103fbf351332f86ff1
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:51:47 +0300

    Update Personal.cpp

--- src/Personal.cpp
+++ src/Personal.cpp
@@ -1,42 +1,43 @@
 #include "Personal.h"
+#include <string>
 
 std::string PositionString[] = {
-	"Driver", "Cleaner"
+    "Driver", "Cleaner"
 };
 
-
-Personal::Personal(unsigned int id, std::string name, Position position, unsigned int salary) :
-	Employee(id, name, position) {
-	this->salary = salary;
+Personal::Personal(int id, std::string name, Position position,
+    int salary) : Employee(id, name, position) {
+    this->salary = salary;
 }
 
-unsigned int Personal::calcBase(unsigned int salary, unsigned int wtime) {
-	return wtime * salary;
+int Personal::calcBase(int salary, int wtime) {
+    return wtime * salary;
 }
 
-unsigned int Personal::calcBonus() {
-	return 200;
+int Personal::calcBonus() {
+    return 200;
 }
 
 void Personal::printInfo() {
-	std::cout << "id " << this->id << std::endl;
-	std::cout << "name " << this->name << std::endl;
-	std::cout << "position " << PositionString[int(this->position)] << std::endl;
-	std::cout << "work time " << this->worktime << std::endl;
-	std::cout << "payment " << this->payment << std::endl;
-	std::cout << std::endl;
+    std::cout << "id " << this->id << std::endl;
+    std::cout << "name " << this->name << std::endl;
+    std::cout << "position " <<
+        PositionString[int(this->position)]<< std::endl;
+    std::cout << "work time " << this->worktime << std::endl;
+    std::cout << "payment " << this->payment << std::endl;
+    std::cout << std::endl;
 }
 
-Driver::Driver(unsigned int id, std::string name, Position position, unsigned int salary) :
-	Personal(id, name, position, salary) {};
+Driver::Driver(int id, std::string name, Position position, int salary) :
+    Personal(id, name, position, salary) {};
 
 void Driver::calc() {
-	this->payment = calcBonus() + calcBase(this->salary, this->worktime);
+    this->payment = calcBonus() + calcBase(this->salary, this->worktime);
 }
 
-Cleaner::Cleaner(unsigned int id, std::string name, Position position, unsigned int salary) :
-	Personal(id, name, position, salary) {};
+Cleaner::Cleaner(int id, std::string name, Position position, int salary) :
+    Personal(id, name, position, salary) {};
 
 void Cleaner::calc() {
-	this->payment = calcBase(this->salary, this->worktime);
+    this->payment = calcBase(this->salary, this->worktime);
 }
