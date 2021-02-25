242a5baef8dcf7d4ba88e9667ee5ebed9782f8be
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:58:09 +0300

    Update Factory.cpp

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -1,48 +1,54 @@
 #include "Factory.h"
 
-
 std::vector<Employee*> makeStaff() {
-	std::ifstream data_file("data.csv");
-	std::vector<Employee*> employee;
-	if (data_file.is_open()) {
-		std::string str = "";
-		getline(data_file, str, '\n');
-		while (data_file.good()) {
-			str.clear();
-			std::string id, name, position, project, salary;
-			getline(data_file, id, ';');
-			getline(data_file, name, ';');
-			getline(data_file, position, ';');
-			getline(data_file, salary, ';');
-			getline(data_file, project, '\n');
+    std::ifstream data_file("data.csv");
+    std::vector<Employee*> employee;
+    if (data_file.is_open()) {
+        std::string str = "";
+        getline(data_file, str, '\n');
+        while (data_file.good()) {
+            str.clear();
+            std::string id, name, position, project, salary;
+            getline(data_file, id, ';');
+            getline(data_file, name, ';');
+            getline(data_file, position, ';');
+            getline(data_file, salary, ';');
+            getline(data_file, project, '\n');
+            if (position == "driver") {
+                employee.push_back(new Driver(stoi(id), name,
+                    Position::DRIVER, stoi(salary)));
+            }
+            else if (position == "cleaner") {
+                employee.push_back(new Cleaner(stoi(id), name,
+                    Position::CLEANER, stoi(salary)));
+            }
+            else if (position == "project manager") {
+                employee.push_back(new ProjectManager(stoi(id),
+                    name, Position::PROJECT_MANAGER, project));
+            }
+            else if (position == "senior manager") {
+                employee.push_back(new SeniorManager(stoi(id),
+                    name, Position::SENIOR_MANAGER, project));
+            }
+            else if (position == "engineer") {
+                employee.push_back(new Engineer(stoi(id), name,
+                    Position::ENGINEER, project, stoi(salary)));
+            }
+            else if (position == "tester") {
+                employee.push_back(new Tester(stoi(id), name,
+                    Position::ENGINEER, project, stoi(salary)));
+            }
+            else if (position == "programmer") {
+                employee.push_back(new Programmer(stoi(id), name,
+                    Position::PROGRAMMER, project, stoi(salary)));
+            }
+            else if (position == "team lead") {
+                employee.push_back(new TeamLead(stoi(id), name,
+                    Position::TEAM_LEAD, project, stoi(salary)));
+            }
 
-			if (position == "driver") {
-				employee.push_back(new Driver(stoi(id), name, Position::DRIVER, stoi(salary)));
-			}
-			else if (position == "cleaner") {
-				employee.push_back(new Cleaner(stoi(id), name, Position::CLEANER,stoi(salary)));
-			}
-			else if (position == "project manager") {
-				employee.push_back(new ProjectManager(stoi(id), name, Position::PROJECT_MANAGER, project));
-			}
-			else if (position == "senior manager") {
-				employee.push_back(new SeniorManager(stoi(id), name, Position::SENIOR_MANAGER, project));
-			}
-			else if (position == "engineer") {
-				employee.push_back(new Engineer(stoi(id), name, Position::ENGINEER, project, stoi(salary)));
-			}
-			else if (position == "tester") {
-				employee.push_back(new Tester(stoi(id), name, Position::ENGINEER, project, stoi(salary)));
-			}
-			else if (position == "programmer") {
-				employee.push_back(new Programmer(stoi(id), name, Position::PROGRAMMER, project, stoi(salary)));
-			}
-			else if (position == "team lead") {
-				employee.push_back(new TeamLead(stoi(id), name, Position::TEAM_LEAD, project, stoi(salary)));
-			}
-
-		}
-	}
-	data_file.close();
-	return employee;
+        }
+    }
+    data_file.close();
+    return employee;
 }
