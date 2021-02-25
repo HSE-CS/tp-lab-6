0318f1889bc2d674ffe45e5c38d399ab0a8607a7
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 21:27:43 +0300

    Update Personal.cpp

--- src/Personal.cpp
+++ src/Personal.cpp
@@ -1,9 +1,6 @@
+// Copyright 2021 by Fatin Maxim
 #include "Personal.h"
 #include <string>
-
-std::string PositionString[] = {
-    "Driver", "Cleaner"
-};
 
 Personal::Personal(int id, std::string name, Position position,
     int salary) : Employee(id, name, position) {
@@ -19,24 +16,29 @@ int Personal::calcBonus() {
 }
 
 void Personal::printInfo() {
+    std::string PositionString[] = {
+    "Driver", "Cleaner"
+    };
     std::cout << "id " << this->id << std::endl;
     std::cout << "name " << this->name << std::endl;
-    std::cout << "position " <<
-        PositionString[int(this->position)]<< std::endl;
+    std::cout <<
+        "position " <<
+        PositionString[static_cast<int>(this->position)]<<
+        std::endl;
     std::cout << "work time " << this->worktime << std::endl;
     std::cout << "payment " << this->payment << std::endl;
     std::cout << std::endl;
 }
 
 Driver::Driver(int id, std::string name, Position position, int salary) :
-    Personal(id, name, position, salary) {};
+    Personal(id, name, position, salary) {}
 
 void Driver::calc() {
     this->payment = calcBonus() + calcBase(this->salary, this->worktime);
 }
 
 Cleaner::Cleaner(int id, std::string name, Position position, int salary) :
-    Personal(id, name, position, salary) {};
+    Personal(id, name, position, salary) {}
 
 void Cleaner::calc() {
     this->payment = calcBase(this->salary, this->worktime);
