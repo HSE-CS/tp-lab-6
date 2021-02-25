a967ea448440da787e2faa17dddaf8fb1bb2c6e5
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 21:11:35 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,108 +8,108 @@
 #include "Employee.h"
 
 TEST(test_cleaner, test1) {
-  Employee* employee = new Cleaner(1, "Igor", Position::DRIVER, 20);
-  employee.setWorkTime(20);
-  std::string name = employee->getName();
-  EXPECT_EQ("Igor", name);
+    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    employee.setWorkTime(20);
+    std::string name = employee->getName();
+    EXPECT_EQ("Igor", name);
 }
 
 TEST(test_cleaner, test2) {
-  Employee* employee = new Cleaner(1, "Igor", Position::DRIVER, 20);
-  employee.setWorkTime(20);
-  int position = int(employee->getPosition());
-  EXPECT_EQ(position, 1);
+    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    employee.setWorkTime(20);
+    int position = int(employee->getPosition());
+    EXPECT_EQ(position, 1);
 }
 
 TEST(test_cleaner, test3) {
-  Employee* employee = new Cleaner(1, "Igor", Position::DRIVER, 20);
-  employee.setWorkTime(20);
-  int time =  employee->getWorktime();
-  EXPECT_EQ(time, 20);
+    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    employee.setWorkTime(20);
+    int time =  employee->getWorktime();
+    EXPECT_EQ(time, 20);
 }
 
 TEST(test_cleaner, test4) {
-  Employee* employee = new Cleaner(1, "Igor", Position::DRIVER, 20);
-  employee.setWorkTime(20);
-  int payment =  employee->getPayment();
-  EXPECT_EQ(payment, 600);
+    Cleaner employee(1, "Igor", Position::DRIVER, 20);
+    employee.setWorkTime(20);
+    int payment =  employee->getPayment();
+    EXPECT_EQ(payment, 600);
 }
 
 TEST(test_manager, test5) {
-  Employee* employee = new ProjectManager(1, "Ivan", Position::PROJECT_MANAGER, 30);
-  employee.setWorkTime(20);
-  int payment =  employee->getPayment();
-  EXPECT_EQ(payment, 69000);
+    ProjectManager employee(1, "Ivan", Position::PROJECT_MANAGER, 30);
+    employee.setWorkTime(20);
+    int payment =  employee->getPayment();
+    EXPECT_EQ(payment, 69000);
 }
 
 TEST(test_manager, test6) {
-  Employee* employee = new ProjectManager(1, "Ivan", Position::PROJECT_MANAGER, 30);
-  employee.setWorkTime(20);
-  std::string name  =  employee->getName();
-  EXPECT_EQ(name, "Ivan");
+    ProjectManager employee(1, "Ivan", Position::PROJECT_MANAGER, 30);
+    employee.setWorkTime(20);
+    std::string name  =  employee->getName();
+    EXPECT_EQ(name, "Ivan");
 }
 
 TEST(test_manager, test7) {
-  Employee* employee = new ProjectManager(1, "Ivan", Position::PROJECT_MANAGER, 30);
-  employee.setWorkTime(20);
-  int time =  employee->getWorktime();
-  EXPECT_EQ(time, 20);
+    ProjectManager employee(1, "Ivan", Position::PROJECT_MANAGER, 30);
+    employee.setWorkTime(20);
+    int time =  employee->getWorktime();
+    EXPECT_EQ(time, 20);
 }
 
 TEST(test_manager, test8) {
-  Employee* employee = new ProjectManager(1, "Ivan", Position::PROJECT_MANAGER, 30);
-  employee.setWorkTime(20);
-  int position =  int(employee->getPosition());
-  EXPECT_EQ(position, 2);
+    ProjectManager employee(1, "Ivan", Position::PROJECT_MANAGER, 30);
+    employee.setWorkTime(20);
+    int position =  int(employee->getPosition());
+    EXPECT_EQ(position, 2);
 }
 
 TEST(test_lead, test9) {
-  Employee* employee = new TeamLead(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
-  employee.setWorkTime(10);
-  int position =  int(employee->getPosition());
-  EXPECT_EQ(position, 7);
+    TeamLead employee(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
+    employee.setWorkTime(10);
+    int position =  int(employee->getPosition());
+    EXPECT_EQ(position, 7);
 }
 
 TEST(test_lead, test10) {
-  Employee* employee = new TeamLead(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
-  employee.setWorkTime(10);
-  std::string name =  employee->getName();
-  EXPECT_EQ(name, "Max");
+    TeamLead employee(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
+    employee.setWorkTime(10);
+    std::string name =  employee->getName();
+    EXPECT_EQ(name, "Max");
 }
 
 TEST(test_lead, test11) {
-  Employee* employee = new TeamLead(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
-  employee.setWorkTime(10);
-  int time =  employee->getWorktime();
-  EXPECT_EQ(time, 10);
+    TeamLead employee(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
+    employee.setWorkTime(10);
+    int time =  employee->getWorktime();
+    EXPECT_EQ(time, 10);
 }
 
 TEST(test_lead, test12) {
-  Employee* employee = new TeamLead(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
-  employee.setWorkTime(10);
-  int payment =  employee->getPayment();
-  EXPECT_EQ(payment, 137000);
+    TeamLead employee(1, "Max", Position::TEAM_LEAD,"name of project", 1200);
+    employee.setWorkTime(10);
+    int payment =  employee->getPayment();
+    EXPECT_EQ(payment, 137000);
 }
 
 TEST(test_enginer, test13) {
-  Employee* employee = new Engineer(1, "Denis", Position::ENGINEER,"name of project", 800);
-  employee.setWorkTime(10);
-  int payment =  employee->getPayment();
-  EXPECT_EQ(payment, 18000);
+    Engineer employee(1, "Denis", Position::ENGINEER,"name of project", 800);
+    employee.setWorkTime(10);
+    int payment =  employee->getPayment();
+    EXPECT_EQ(payment, 18000);
 }
 
 TEST(test_enginer, test14) {
-  Employee* employee = new Engineer(1, "Denis", Position::ENGINEER,"name of project", 800);
-  employee.setWorkTime(10);
-  int time =  employee->getWorktime();
-  EXPECT_EQ(time, 10);
+    Engineer employee(1, "Denis", Position::ENGINEER,"name of project", 800);
+    employee.setWorkTime(10);
+    int time =  employee->getWorktime();
+    EXPECT_EQ(time, 10);
 }
 
 TEST(test_enginer, test15) {
-  Employee* employee = new Engineer(1, "Denis", Position::ENGINEER,"name of project", 800);
-  employee.setWorkTime(10);
-  int position =  int(employee->getPosition());
-  EXPECT_EQ(position, 4);
+    Engineer employee(1, "Denis", Position::ENGINEER,"name of project", 800);
+    employee.setWorkTime(10);
+    int position =  int(employee->getPosition());
+    EXPECT_EQ(position, 4);
 }
 
 
