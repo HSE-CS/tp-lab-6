8c70e99ec780ffdeda5d701c790cd05f759530e8
skprpi <60091575+skprpi@users.noreply.github.com>
2021-02-25 20:37:19 +0300

    Update Manager.h

--- include/Manager.h
+++ include/Manager.h
@@ -1,33 +1,33 @@
-#ifndef MANAGER_H
-#define MANAGER_H
+// Copyright 2021 by Fatin Maxim
+#ifndef INCLUDE_MANAGER_H_
+#define INCLUDE_MANAGER_H_
 
 #include "Employee.h"
 #include "Interfaces.h"
 #include <iostream>
 #include <string>
 
-class ProjectManager : public Employee, public ProjectBudget, public Project, public Heading{
-protected:
-	Project project;
-public:
-	ProjectManager(unsigned int id, std::string name, Position position, std::string project);
-	unsigned int calcHeads();
-	void calc();
-	unsigned int calcProAdditions();
-	unsigned int calcBudgetPart(double part, unsigned int budget);
-	void printInfo();
+class ProjectManager : public Employee, public ProjectBudget,
+	public Project, public Heading{
+ protected:
+    Project project;
+ public:
+    ProjectManager(unsigned int id, std::string name,
+        Position position, std::string project);
+    int calcHeads();
+    void calc();
+    int calcProAdditions();
+    int calcBudgetPart(double part, int budget);
+    void printInfo();
 };
-
-
-
 
 class SeniorManager : public ProjectManager{
-protected:
-	Project project;
-public:
-	SeniorManager(unsigned int id, std::string name, Position position, std::string project);
-	void calc();
-	unsigned int calcBudgetPart(double part, unsigned int budget);
+ protected:
+    Project project;
+ public:
+    SeniorManager(unsigned int id, std::string name, Position position, std::string project);
+    void calc();
+    int calcBudgetPart(double part, int budget);
 };
 
-#endif
+#endif  // INCLUDE_MANAGER_H_
