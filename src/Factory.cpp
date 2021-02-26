2213f85e719ed3bc54427be8dc0e59478d46c0a4
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-02-26 18:23:15 +0300

    fixing typos

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -18,7 +18,8 @@ std::vector<Employee*> makeStaff(std::string input) {
         }
         while (file.good()) {
             buf.clear();
-            std::string id, fio, position, payment, project_id, project_budget;
+            std::string id, fio, position, payment;
+            std::string project_id, project_budget;
             getline(file, id, ';');
             getline(file, fio, ';');
             getline(file, position, ';');
@@ -27,34 +28,32 @@ std::vector<Employee*> makeStaff(std::string input) {
             getline(file, project_budget, '\n');
 
             if (position == "tester") {
-                Project* project = new Project(stoi(project_id), double(stoi(project_budget)));
+                Project* project = new Project(stoi(project_id),
+                                    double(stoi(project_budget)));
                 employee.push_back(new Tester(stoi(id), fio,
                     position, double(stoi(payment)), project));
-            }
-            else if (position == "programmer") {
-                Project* project = new Project(stoi(project_id), double(stoi(project_budget)));
+            } else if (position == "programmer") {
+                Project* project = new Project(stoi(project_id),
+                                    double(stoi(project_budget)));
                 employee.push_back(new Programmer(stoi(id), fio,
                     position, double(stoi(payment)), project));
-            }
-            else if (position == "teamleader") {
-                Project* project = new Project(stoi(project_id), double(stoi(project_budget)));
+            } else if (position == "teamleader") {
+                Project* project = new Project(stoi(project_id),
+                                     double(stoi(project_budget)));
                 employee.push_back(new TeamLeader(stoi(id), fio,
                     position, double(stoi(payment)), project));
-            }
-            else if (position == "cleaner") {
+            } else if (position == "cleaner") {
                 employee.push_back(new Cleaner(stoi(id), fio,
                     position, double(stoi(payment))));
-            }
-            else if (position == "driver") {
+            } else if (position == "driver") {
                 employee.push_back(new Driver(stoi(id), fio,
                     position, double(stoi(payment))));
-            }
-            else if (position == "project manager") {
-                Project* project = new Project(stoi(project_id), double(stoi(project_budget)));
+            } else if (position == "project manager") {
+                Project* project = new Project(stoi(project_id),
+                                      double(stoi(project_budget)));
                 employee.push_back(new ProjectManager(stoi(id), fio,
                     position, double(stoi(payment)), project));
-            }
-            else if (position == "senior manager") {
+            } else if (position == "senior manager") {
                 std::vector <Project*> projects;
                 employee.push_back(new SeniorManager(stoi(id), fio,
                     position, double(stoi(payment)), projects));
