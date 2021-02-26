0355b0839415d1813175c5ea9163e726715f2e36
Ksuvot <ksuvot36@yandex.ru>
2021-02-26 15:52:29 +0300

    add tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,3 +8,19 @@ TEST(test1, test1) {
   Driver driver1(1, "name", 100);
   EXPECT_EQ(100, driver1.calcBonus());
 }
+
+TEST(test2, test2) {
+  Driver driver1(1, "name", 100);
+  EXPECT_EQ(driver1.getPayment(), driver1.calc());
+}
+
+TEST(test3, test3) {
+  Driver driver1(1, "name", 100);
+  EXPECT_EQ("name", driver1.getName());
+}
+
+TEST(test4, test4) {
+  Driver driver1(1, "name", 100);
+  driver1.setSalary(90);
+  EXPECT_EQ(90, driver1.getSalary());
+}
