47054edaec0a3b560b03f40ab175bd50e26a522e
Ksuvot <ksuvot36@yandex.ru>
2021-02-26 14:45:41 +0300

    add 4 tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,9 @@
 #include "Factory.h"
 
 int main() {
+  std::string name = "Name";
+  Employee employee();
+  employee.setId(1);
+  employee.setName(name);
   return 0;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,28 @@
 
 #include <gtest/gtest.h>
 #include <string>
+#include "Factory.h"
 
+TEST(test1, test1) {
+  Employee employee();
+  employee.setId(123);
+  EXPECT_EQ(123, employee.getId());
+}
 
+TEST(test2, test2) {
+  Employee employee();
+  employee.setName("name");
+  EXPECT_EQ("name", employee.getName());
+}
+
+TEST(test3, test3) {
+  Employee employee();
+  employee.setWorktime(168);
+  EXPECT_EQ(168, employee.getWorktime());
+}
+
+TEST(test4, test4) {
+  Employee employee();
+  employee.setPayment(25000);
+  EXPECT_EQ(25000, employee.getPayment());
+}
