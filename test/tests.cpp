968bd1ae6fde587d18ce537e687754927279dda0
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2021-02-26 21:32:37 +0300

    more tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -33,3 +33,93 @@ TEST(TestDriver, getName) {
   EXPECT_EQ("Vasilii", d.getName());
 }
 
+TEST(TestProgrammer, getPos) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  Programmer d(156, n, 500, &p);
+  EXPECT_EQ(d.getPos(), PROGRAMMER);
+}
+
+TEST(TestProgrammer, calc) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  Programmer d(156, n, 500, &p);
+  d.setWorkTime(200);
+  d.calc();
+  EXPECT_EQ(d.getPayment(), 500 * 200 + 15000);
+}
+
+TEST(TestProgrammer, getProject) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  Programmer d(156, n, 500, &p);
+  EXPECT_EQ(d.getProject(), &p);
+}
+
+TEST(TestTester, getPos) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  Tester d(156, n, 500, &p);
+  EXPECT_EQ(d.getPos(), TESTER);
+}
+
+TEST(TestTester, calc) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  Tester d(156, n, 500, &p);
+  d.setWorkTime(200);
+  d.incErrorsFound();
+  d.incErrorsFound();
+  d.incErrorsFound();
+  d.calc();
+  EXPECT_EQ(d.getPayment(), 500 * 200 + 300 * 3);
+}
+
+TEST(TeamLeader, getPos) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  TeamLeader d(156, n, 500, &p, 10);
+  EXPECT_EQ(d.getPos(), TEAMLEADER);
+}
+
+TEST(TeamLeader, getName) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  TeamLeader d(156, n, 500, &p, 10);
+  EXPECT_EQ(d.getName(), "Vasilii");
+}
+
+TEST(TeamLeader, getSubb) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  TeamLeader d(156, n, 500, &p, 10);
+  EXPECT_EQ(d.getSubbordinates(), 10);
+}
+
+TEST(TeamLeader, calc) {
+  Project p;
+  p.id = 3030;
+  p.budget = 10000;
+  std::string n = "Vasilii";
+  TeamLeader d(156, n, 800, &p, 10);
+  d.setWorkTime(200);
+  d.calc();
+  EXPECT_EQ(d.getPayment(), 800 * 200 + 15000 + 10 * 1000);
+}
+
