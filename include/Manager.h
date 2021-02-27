603e7cdecc44b4401dabdcfd7f13898b8db20f79
alexgiving <alexgiving@mail.ru>
2021-02-28 00:26:22 +0300

    add Manager.h/cpp (ProjectManager class, SeniorManager class)

---
+++ include/Manager.h
@@ -0,0 +1,35 @@
+// Copyright 2021 alexgiving
+
+#ifndef INCLUDE_MANAGER_H_
+#define INCLUDE_MANAGER_H_
+
+#include <vector>
+#include "Personal.h"
+#include "Project.h"
+
+class ProjectManager : public Personal, public Project {
+ private:
+  float part = 0;
+  size_t heads = 0;
+  std::vector<Project*> *projects{};
+
+ public:
+  ProjectManager();
+  ProjectManager(size_t, std::string, size_t, Project* proj, size_t);
+  void setProject(Project*);
+  void setPart(float);
+  void calcHeads(size_t);
+  float calcPM();
+  std::vector<Project*>* getProjects();
+  [[nodiscard]] float getPart() const;
+  [[nodiscard]] size_t getHeads() const;
+};
+
+class SeniorManager : public ProjectManager {
+ public:
+  SeniorManager();
+  SeniorManager(size_t, std::string, size_t, std::vector<Project *>*);
+  size_t calc();
+};
+
+#endif // INCLUDE_MANAGER_H_
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "Cleaner.h"
 #include "Driver.h"
 #include "Personal.h"
+#include "Manager.h"
 
 int main() {
   return 0;
