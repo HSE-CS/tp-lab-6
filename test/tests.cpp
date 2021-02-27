0438750df6941e42ba42d010f1c2e71628091b60
alexgiving <alexgiving@mail.ru>
2021-02-28 01:35:22 +0300

    delete string

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,50 +5,42 @@
 #include "Interfaces.h"
 
 TEST(Test, test_1) {
-  std::string name = "Ivan";
-  Cleaner test(0, name, 8, 2, 500);
+  Cleaner test(0, "Ivan", 8, 2, 500);
   EXPECT_EQ(0, test.getId());
 }
 
 TEST(Test, test_2) {
-  std::string name = "Ivan";
-  Cleaner test(10, name, 8, 2, 500);
+  Cleaner test(10, "Ivan", 8, 2, 500);
   EXPECT_EQ(10, test.getId());
 }
 
 TEST(Test, test_3) {
-  std::string name = "Ivan";
-  Cleaner test(225, name, 8, 2, 500);
+  Cleaner test(225, "Ivan", 8, 2, 500);
   EXPECT_EQ(225, test.getId());
 }
 
 TEST(Test, test_4) {
-  std::string name = "Ivan";
-  Cleaner test(0, name, 8, 2, 500);
-  EXPECT_EQ(name, test.getName());
+  Cleaner test(0, "Ivan", 8, 2, 500);
+  EXPECT_EQ("Ivan", test.getName());
 }
 
 TEST(Test, test_5) {
-  std::string name = "Ivan";
-  Cleaner test(0, name, 8, 2, 500);
+  Cleaner test(0, "Ivan", 8, 2, 500);
   EXPECT_EQ(8, test.getWorktime());
 }
 
 TEST(Test, test_6) {
-  std::string name = "Ivan";
-  Cleaner test(0, name, 8, 2, 500);
+  Cleaner test(0, "Ivan", 8, 2, 500);
   EXPECT_EQ(500, test.getPayment());
 }
 
 TEST(Test, test_7) {
-  std::string name = "Ivan";
-  Cleaner test(0, name, 8, 2, 500);
+  Cleaner test(0, "Ivan", 8, 2, 500);
   EXPECT_EQ("CLEANER", test.getPosition());
 }
 
 TEST(Test, test_8) {
-  std::string name = "Ivan";
-  Driver test(0, name, 8, 2, 500);
+  Driver test(0, "Ivan", 8, 2, 500);
   EXPECT_EQ("DRIVER", test.getPosition());
 }
 
@@ -64,27 +56,23 @@ TEST(ProjectTest, test_10) {
 
 
 TEST(ProjectTest, test_11) {
-  std::string name = "Ivan";
   Project testP();
-  Programmer test(11, name, 8, &proj, 100000, 0,5);
-  EXPECT_EQ(0,5, test.getPart());
+  Programmer test(11, "Ivan", 8, &proj, 100000, 0.5);
+  EXPECT_EQ(0.5, test.getPart());
 }
 
 TEST(ProjectTest, test_12) {
-  std::string name = "Ivan";
   Project testP();
-  Programmer test(11, name, 8, &proj, 100000, 0,5);
+  Programmer test(11, "Ivan", 8, &proj, 100000, 0.5);
   EXPECT_EQ(0, proj.getProjectId());
 }
 
 TEST(ProjectTest, test_12) {
-  std::string name = "Ivan";
-  Driver test(11, name, 8, 5);
+  Driver test(11, "Ivan", 8, 5);
   EXPECT_EQ(40, test.calcBase());
 }
 
 TEST(ProjectTest, test_13) {
-  std::string name = "Ivan";
-  Driver test(11, name, 0, 1000);
+  Driver test(11, "Ivan", 0, 1000);
   EXPECT_EQ(0, test.calcBase());
 }
