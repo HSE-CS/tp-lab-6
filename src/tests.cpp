647bcc49c8236b257df3c845cdb15290ea15cd8a
GlebNeshchetkin <glebneshchetkin@gmail.com>
2021-02-27 22:20:26 +0300

    FirstTest

--- src/tests.cpp
+++ src/tests.cpp
@@ -52,55 +52,55 @@ TEST(TestTester, Test_5) {
     Tester pr(10, name_, 100, &project_1);
     pr.setWorkTime(2);
     pr.calc();
-	EXPECT_EQ(400, pr.getPayment());
+    EXPECT_EQ(400, pr.getPayment());
 }
 
 TEST(TestTester, Test_6) {
-	std::string name_ = "Ivanov Ivan";
-	Project project_1;
-	project_1.id = "project_1";
-	project_1.budget = 20000;
-	Tester pr(10, name_, 100, &project_1);
-	pr.setWorkTime(2);
-	pr.newError();
-	pr.calc();
-	EXPECT_EQ(500, pr.getPayment());
+    std::string name_ = "Ivanov Ivan";
+    Project project_1;
+    project_1.id = "project_1";
+    project_1.budget = 20000;
+    Tester pr(10, name_, 100, &project_1);
+    pr.setWorkTime(2);
+    pr.newError();
+    pr.calc();
+    EXPECT_EQ(500, pr.getPayment());
 }
 
 TEST(TestTeamLeader, Test_7) {
-	std::string name_ = "Ivanov Ivan";
-	Project project_1;
-	project_1.id = "project_1";
-	project_1.budget = 20000;
-	TeamLeader pr(10, name_, 200, &project_1);
-	pr.setWorkTime(2);
-	pr.calc();
-	EXPECT_EQ(800, pr.getPayment());
+    std::string name_ = "Ivanov Ivan";
+    Project project_1;
+    project_1.id = "project_1";
+    project_1.budget = 20000;
+    TeamLeader pr(10, name_, 200, &project_1);
+    pr.setWorkTime(2);
+    pr.calc();
+    EXPECT_EQ(800, pr.getPayment());
 }
 
 TEST(TestTeamLeader, Test_8) {
-	std::string name_ = "Ivanov Ivan";
-	Project project_1;
-	project_1.id = "project_1";
-	project_1.budget = 20000;
-	TeamLeader pr(10, name_, 200, &project_1);
-	pr.setWorkTime(2);
-	pr.addSubordinate();
-	pr.calc();
-	EXPECT_EQ(1800, pr.getPayment());
+    std::string name_ = "Ivanov Ivan";
+    Project project_1;
+    project_1.id = "project_1";
+    project_1.budget = 20000;
+    TeamLeader pr(10, name_, 200, &project_1);
+    pr.setWorkTime(2);
+    pr.addSubordinate();
+    pr.calc();
+    EXPECT_EQ(1800, pr.getPayment());
 }
 
 TEST(TestProjectManager, Test_9) {
-	std::string name_ = "Ivanov Ivan";
-	Project project_1;
-	project_1.id = "project_1";
-	project_1.budget = 2000000;
-	ProjectManager pr(10, name_, &project_1);
-	pr.setWorkTime(2);
-	pr.addSubordinates();
-	pr.addSubordinates();
-	pr.calc();
-	EXPECT_EQ(2090, pr.getPayment());
+    std::string name_ = "Ivanov Ivan";
+    Project project_1;
+    project_1.id = "project_1";
+    project_1.budget = 2000000;
+    ProjectManager pr(10, name_, &project_1);
+    pr.setWorkTime(2);
+    pr.addSubordinates();
+    pr.addSubordinates();
+    pr.calc();
+    EXPECT_EQ(2090, pr.getPayment());
 }
 
 TEST(TestSeniorManager, Test_10) {
@@ -117,11 +117,11 @@ TEST(TestSeniorManager, Test_10) {
 }
 
 TEST(TestCleaner, Test_11) {
-	std::string name_ = "Ivan Ivanov";
-	Cleaner c(156, name_, 50);
-	c.setWorkTime(2);
-	c.calc();
-	EXPECT_EQ(100, c.getPayment());
+    std::string name_ = "Ivan Ivanov";
+    Cleaner c(156, name_, 50);
+    c.setWorkTime(2);
+    c.calc();
+    EXPECT_EQ(100, c.getPayment());
 }
 
 TEST(TestDriver, Test_12) {
