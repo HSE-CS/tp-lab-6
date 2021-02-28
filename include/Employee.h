4883ec14ca467eb82974da0604bf11eb093f3ed1
TimurZaytsev <tz200128@gmail.com>
2021-02-28 22:42:17 +0300

    Sixth

--- include/Employee.h
+++ include/Employee.h
@@ -9,40 +9,40 @@
 typedef uint64_t id_type;
 
 enum class Position {
-	Driver,
-	Cleaner,
-	Tester,
-	Programmer,
-	TeamLeader,
-	ProjectManager,
-	SeniorManager
+ Driver,
+ Cleaner,
+ Tester,
+ Programmer,
+ TeamLeader,
+ ProjectManager,
+ SeniorManager
 };
 
 
 struct Project {
-	std::string _id;
-	int _budget;
+ std::string _id;
+ int _budget;
 };
 
 
 class Employee {
- public:
-	Employee(id_type id, const std::string& name);
-	virtual ~Employee() = default;
+public:
+ Employee(id_type id, const std::string& name);
+ virtual ~Employee() = default;
 
-	void worktime(unsigned int wt);
+ void worktime(unsigned int wt);
 
-	virtual void calc() = 0;
-	virtual void print_info() = 0;
+ virtual void calc() = 0;
+ virtual void print_info() = 0;
 
-	friend class StaffFactory;
+ friend class StaffFactory;
 
- protected:
-	id_type _id;
-	std::string _nameof;
-	Position _position;
-	unsigned int _timeofwork;
-	unsigned int _payment;
+protected:
+ id_type _id;
+ std::string _nameof;
+ Position _position;
+ unsigned int _timeofwork;
+ unsigned int _payment;
 
 };
 
