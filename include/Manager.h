30847c87042849d8a852c03c86f8412c59b7864c
TimurZaytsev <tz200128@gmail.com>
2021-02-28 23:17:59 +0300

    Fourteenth

--- include/Manager.h
+++ include/Manager.h
@@ -11,28 +11,27 @@
 
 
 class ProjectManager : public Employee, public Heading {
-public:
-	ProjectManager(id_type id, const std::string& name, const Project& p);
-	~ProjectManager() override = default;
+ public:
+ProjectManager(id_type id, const std::string& name, const Project& p);
+~ProjectManager() override = default;
+void calc() override;
+void print_info() override;
+int calc_heads() override;
 
-	void calc() override;
-	void print_info() override;
-	int calc_heads() override;
-
-private:
-	Project _project;
+ private:
+Project _project;
 };
 
 
 class SeniorManager final : public ProjectManager {
-public:
-	SeniorManager(id_type id, const std::string& name, std::vector<Project> ps);
+ public:
+SeniorManager(id_type id, const std::string& name, std::vector<Project> ps);
 
-	void calc() override;
-	void print_info() override;
+void calc() override;
+void print_info() override;
 
-private:
-	std::vector<Project> _projects;
+ private:
+std::vector<Project> _projects;
 };
 
 #endif  // INCLUDE_MANAGER_H_
