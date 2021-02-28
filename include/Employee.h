4fd72b802a97e17827a9679502c39dff1eeff110
Your Name <olga.frolova.2001@bk.ru>
2021-02-28 12:31:44 +0300

    first try

---
+++ include/Employee.h
@@ -0,0 +1,35 @@
+// Copyright 2021 FOM
+#ifndef INCLUDE_EMPLOYEE_H_
+#define INCLUDE_EMPLOYEE_H_
+#include <string>
+
+enum class Position {
+    DRIVER,
+    CLEANER,
+    PROGRAMMER,
+    TESTER,
+    TEAMLEADER,
+    PROJECTMANAGER,
+    SENIORMANAGER
+};
+
+class Employee {
+ protected:
+    int id;
+    std::string name;
+    std::string position;
+    int worktime;
+    int payment;
+ public:
+    Employee(int id, std::string name,
+        std::string position, int worktime, int payment);
+    int getId();
+    std::string getName();
+    std::string getPosition();
+    void setWorkTime(size_t wtime);
+    int getWorkTime();
+    int getPayment();
+    virtual int calc() = 0;
+    virtual void printInfo() = 0;
+};
+#endif  // INCLUDE_EMPLOYEE_H_
---
+++ src/Engineer.cpp
@@ -0,0 +1,75 @@
+// Copyright 2021 FOM
+#include "Engineer.h"
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+int Engineer::calcBudgetPart(float part, int budget) {
+    return part * budget;
+}
+double Engineer::getPart() {
+    return part;
+}
+int Engineer::calc() {
+    return Personal::calc();
+}
+void Engineer::printInfo() {
+    std::cout << "ID : " << getId() << std::endl;
+    std::cout << "Name : " << getName() << std::endl;
+    std::cout << "Position : " << getPosition() << std::endl;
+    std::cout << "Work time : " << getWorkTime() << std::endl;
+    std::cout << "Salary : " << getSalary() << std::endl;
+    std::cout << "Payment : " << getPayment() << std::endl;
+}
+
+int Tester::calcProAdditions() {
+    return Personal::calc() * 0.2;
+}
+int Tester::calc() {
+    return Engineer::calc() + calcProAdditions();
+}
+void Tester::printInfo() {
+    std::cout << "ID : " << getId() << std::endl;
+    std::cout << "Name : " << getName() << std::endl;
+    std::cout << "Position : " << getPosition() << std::endl;
+    std::cout << "Work time : " << getWorkTime() << std::endl;
+    std::cout << "Salary : " << getSalary() << std::endl;
+    std::cout << "Payment : " << getPayment() << std::endl;
+}
+
+int Programmer::calcProAdditions() {
+    return Personal::calc() * 0.3;
+}
+int Programmer::calc() {
+    return Engineer::calc() + calcProAdditions();
+}
+
+void Programmer::printInfo() {
+    std::cout << "ID : " << getId() << std::endl;
+    std::cout << "Name : " << getName() << std::endl;
+    std::cout << "Position : " << getPosition() << std::endl;
+    std::cout << "Salary : " << getSalary() << std::endl;
+    std::cout << "Work time : " << getWorkTime() << std::endl;
+    std::cout << "Payment : " << getPayment() << std::endl;
+}
+
+int TeamLeader::calcHeads() { 
+    return 32354; 
+}
+
+int TeamLeader::calc() {
+    return Engineer::calc() + calcProAdditions() + calcHeads();
+}
+
+void TeamLeader::printInfo() {
+    std::cout << "ID : " << getId() << std::endl;
+    std::cout << "Name : " << getName() << std::endl;
+    std::cout << "Position : " << getPosition() << std::endl;
+    std::cout << "Salary : " << getSalary() << std::endl;
+    std::cout << "Work time : " << getWorkTime() << std::endl;
+    std::cout << "Payment : " << getPayment() << std::endl;
+}
