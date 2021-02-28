6ac0e88930eeb5dd032588ba277f491b76126531
SharunovEvgeny <Ee95023495>
2021-02-28 20:37:03 +0300

    First Try

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,15 @@
 
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
+#include "Personal.h"
+#include "Engineer.h"
+#include "Manager.h"
+#include "Project.h"
 
-
+TEST(test_cleaner, test1) {
+Cleaner* cleaner1 = new Cleaner(0,
+                                "ivan", 1000, 2,cleaner);
+int payment = cleaner1->calc();
+EXPECT_EQ(2000, payment);
+}
