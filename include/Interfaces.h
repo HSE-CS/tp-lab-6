5fa635c4a4d1d73219914004faa2a3f6397f10f8
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-03-01 02:41:57 +0300

    Update Interfaces.h

--- include/Interfaces.h
+++ include/Interfaces.h
@@ -7,12 +7,12 @@
 class WorkBaseTime {
  public:
   virtual int calcBase(int hour_salary, int work_time) = 0;
-  virtual int calcBonus() = 0;
+  virtual float calcBonus() = 0;
 };
 
 class ProjectBudget {
  public:
-  virtual int calcBudgetPart(float contrib, int budget) = 0;
+  virtual float calcBudgetPart(float contrib, int budget) = 0;
   virtual int calcProAdditions() = 0;
 };
 
