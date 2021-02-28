77775db23f0bef27102f958df31c4bd48962df22
Sergey Boryaev <somsom.som2@yandex.ru>
2021-02-28 17:49:57 +0300

    fix style

--- include/Interfaces.h
+++ include/Interfaces.h
@@ -4,24 +4,24 @@
 
 class WorkBaseTime {
  public:
-	virtual unsigned calcBase() = 0;
-	virtual unsigned calcBonus(unsigned overtimeJob, unsigned cost) = 0;
+  virtual unsigned calcBase() = 0;
+  virtual unsigned calcBonus(unsigned overtimeJob, unsigned cost) = 0;
 };
 
 class ProjectBudget {
  public:
-	virtual unsigned calcBudgetPart(float part, int budget) = 0;
-	virtual unsigned calcProAdditions() = 0;
+  virtual unsigned calcBudgetPart(float part, int budget) = 0;
+  virtual unsigned calcProAdditions() = 0;
 };
 
 class Heading {
  public:
-	virtual unsigned calcHeads(int numberOfWorkers) = 0;
+  virtual unsigned calcHeads(int numberOfWorkers) = 0;
 };
 
 struct Project {
-	unsigned id;
-	unsigned budget;
+  unsigned id;
+  unsigned budget;
 };
 
 
