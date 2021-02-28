811084c78e998739bc7f30db964b35b6af391133
TimurZaytsev <tz200128@gmail.com>
2021-02-28 22:16:37 +0300

    Second

--- test/tests.cpp
+++ test/tests.cpp
@@ -16,14 +16,14 @@ TEST(StaffDemoTest, test1) {
 
 TEST(StaffDemoTest, test2) {
     TeamLeader tl(21312321, "John Doe", 44000, { "13213", 200000 });
-    tl.set_worktime(24);
+    tl.worktime(24);
 
     EXPECT_EQ(tl.calc_heads(), 220000);
 }
 
 TEST(StaffDemoTest, test3) {
     Tester t(21312321, "John Doe", 44000, { "13213", 200000 });
-    t.set_worktime(24);
+    t.worktime(24);
 
     EXPECT_EQ(t.calc_budget_part(0.1, 100000), 10000);
 }
@@ -55,35 +55,35 @@ TEST(StaffDemoTest, test7) {
 
 TEST(StaffDemoTest, test8) {
     Tester t(21312321, "John Doe", 44000, { "13213", 200000 });
-    t.set_worktime(24);
+    t.worktime(24);
 
     EXPECT_EQ(t.calc_bonus(), 0);
 }
 
 TEST(StaffDemoTest, test9) {
     Tester t(21312321, "John Doe", 44000, { "13213", 200000 });
-    t.set_worktime(24);
+    t.worktime(24);
 
     EXPECT_EQ(t.calc_pro_additions(), 10560);
 }
 
 TEST(StaffDemoTest, test10) {
     Programmer p(21312321, "John Doe", 44000, { "13213", 200000 });
-    p.set_worktime(24);
+    p.worktime(24);
 
     EXPECT_EQ(p.calc_pro_additions(), 18333);
 }
 
 TEST(StaffDemoTest, test11) {
     ProjectManager pm(21312321, "John Doe", { "13213", 200000 });
-    pm.set_worktime(24);
+    pm.worktime(24);
 
     EXPECT_EQ(pm.calc_heads(), 50000);
 }
 
 TEST(StaffDemoTest, test12) {
     Driver d(123123, "John Doe", 1000);
-    d.set_worktime(10);
+    d.worktime(10);
 
     EXPECT_EQ(d.calc_bonus(), 100);
 }
