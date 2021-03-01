9780080d1ee7c2c714724cfa1969ff5789a0b6af
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-03-01 03:15:37 +0300

    Update Manager.h

--- include/Manager.h
+++ include/Manager.h
@@ -15,7 +15,7 @@ class ProjectManager : public Employee, public ProjectBudget, public Heading {
 
  public:
   ProjectManager(int id, std::string name, std::string position, int numProj,
-                 int worktime, int payment, std::vector<Project *> &projects,
+                 int worktime, int payment, const std::vector<Project *> &projects,
                  float contribIn)
       : Employee(id, name, position, worktime, payment), contrib(contribIn) {}
   float calcBudgetPart(float contrib, int budget) override;
@@ -28,7 +28,7 @@ class ProjectManager : public Employee, public ProjectBudget, public Heading {
 class SeniorManager : public ProjectManager {
  public:
   SeniorManager(int id, std::string name, std::string position, int numProj,
-                int worktime, int payment, std::vector<Project *> &projects,
+                int worktime, int payment, const std::vector<Project *> &projects,
                 float contrib)
       : ProjectManager(id, name, position, numProj, worktime, payment, projects,
                        contrib) {}
