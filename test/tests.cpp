de7f6239fc2e5b6fddd5dab1741d909a62cbbd1f
theremin_vox <66740034+alex-linyx1@users.noreply.github.com>
2021-03-08 00:49:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,6 +7,25 @@
 #include"Personal.h"
 #include<vector>
 
+std::string converse_(Position pos) {
+  if (pos == PROGRAMMER)
+    return "Programmer";
+  else if (pos == TESTER)
+    return "Tester";
+  else if (pos == DRIVER)
+    return "Driver";
+  else if (pos == CLEANER)
+    return "Cleaner";
+  else if (pos == ENGINEER)
+    return "Engineer";
+  else if (pos == SENIOR_MANAGER)
+    return "SeniorManager";
+  else if (pos == PROJECT_MANAGER)
+    return "ProjectManager";
+  else if (pos == TEAM_LEADER)
+    return "TeamLeader";
+}
+
 TEST(TESTS, test1) {
   std::string name = "Alex";
   Project project("LALALA", 1000);
@@ -32,7 +51,7 @@ TEST(TESTS, test4) {
   std::string name = "Alex";
   Project project("LALALA", 1000);
   Programmer pr(1, name, PROGRAMMER, 30, &project, 0.3);
-  EXPECT_EQ("Programmer", pr.get_position());
+  EXPECT_EQ("Programmer", converse_(pr.get_position()));
 }
 
 TEST(TESTS, test5) {
@@ -55,7 +74,7 @@ TEST(TESTS, test7) {
   std::string name = "Alex";
   Project project("LALALA", 1000);
   Tester pr(1, name, TESTER, 30, &project, 0.3, 3);
-  EXPECT_EQ("Tester", pr.get_position());
+  EXPECT_EQ("Tester", converse_(pr.get_position()));
 }
 
 TEST(TESTS, test8) {
@@ -76,20 +95,20 @@ TEST(TESTS, test9) {
 TEST(TESTS, test10) {
   Driver dr(1, "Ilya", DRIVER, 30);
   dr.set_work_time(11);
-  EXPECT_EQ("Driver", dr.get_position());
+  EXPECT_EQ("Driver", converse_(dr.get_position()));
 }
 
 TEST(TESTS, test11) {
   std::string name = "Alex";
   Cleaner cl(1, name, CLEANER, 30);
-  EXPECT_EQ("Cleaner", cl.get_position());
+  EXPECT_EQ("Cleaner", converse_(cl.get_position()));
 }
 
 TEST(TESTS, test12) {
   Project project("LALALA", 1000);
   std::string name = "Alex";
   TeamLeader tl(1, name, TEAM_LEADER, 30, &project, 0.3);
-  EXPECT_EQ("TeamLeader", tl.get_position());
+  EXPECT_EQ("TeamLeader", converse_(tl.get_position()));
 }
 
 TEST(TESTS, test13) {
@@ -98,7 +117,7 @@ TEST(TESTS, test13) {
   projects.push_back(&project);
   std::string name = "Alex";
   ProjectManager pt(1, name, PROJECT_MANAGER, projects, "LALALA" );
-  EXPECT_EQ("ProjectManager", pt.get_position());
+  EXPECT_EQ("ProjectManager", converse_(pt.get_position()));
 }
 
 TEST(TESTS, test14) {
@@ -107,7 +126,7 @@ TEST(TESTS, test14) {
   projects.push_back(&project);
   std::string name = "Alex";
   SeniorManager pt(1, name, SENIOR_MANAGER, projects);
-  EXPECT_EQ("SeniorManager", pt.get_position());
+  EXPECT_EQ("SeniorManager", converse_(pt.get_position()));
 }
 
 TEST(TESTS, test15) {
