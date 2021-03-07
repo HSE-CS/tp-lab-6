b36e9deea7a6982938a4bfc40cd7c604a9e2a22e
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2021-03-07 18:05:00 +0300

    b64

---
+++ src/Employee.cpp
@@ -0,0 +1,22 @@
+#include "Employee.h"
+void Employee::setPayment(int payment_) {
+    payment = payment_;
+}
+void Employee::setWorkTime(int worktime_) {
+    worktime = worktime_;
+}
+int Employee::getWorkTime() {
+    return worktime;
+}
+std::string Employee::getName() {
+    return name;
+}
+int Employee::getID() {
+    return id;
+}
+int Employee::getPayment() {
+    return payment;
+}
+position Employee::getPos() {
+    return spec;
+}
