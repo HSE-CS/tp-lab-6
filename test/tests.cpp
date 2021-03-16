a17ec3d36b5aa3b9ff669a33020b2efa2a46ad4d
Julia Rogozyan <juliarogozyan@Air-Julia.mts.local>
2021-03-16 10:58:52 +0300

    Rogozyan4

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,12 +8,13 @@
 
  #include <gtest/gtest.h>
  #include <string>
+#include "Manager.h"
+#include "Personal.h"
  #include "Interfaces.h"
  #include "Employee.h"
  #include "Engineer.h"
  #include "Factory.h"
- #include "Manager.h"
- #include "Personal.h"
+ 
 
 
  TEST(Test_Employee, test_1) {
