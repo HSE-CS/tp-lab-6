9c3997512ef6662c84becb738d17a6565b9945e8
Nik <50590478+stifeevn@users.noreply.github.com>
2021-03-24 10:01:54 +0500

    Update Manager.cpp

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -9,7 +9,7 @@ ProjectManager::ProjectManager(int id, std::string name) :
 }
 
 int ProjectManager::calcBudgetPart(float part, int budget) {
-    return static_cast<float>(part * (float)(budget));
+    return static_cast<float>(part * static_cast<float>(budget));
 }
 
 int ProjectManager::calcHeads() {
