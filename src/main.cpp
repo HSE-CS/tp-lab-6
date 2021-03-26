d1558456dfed746c02335291dcdd294c55cd1425
Дима булатов <dabulatov@edu.hse.ru>
2021-03-26 19:55:57 +0300

    change the approach

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,17 @@
 #include "Factory.h"
 
 int main() {
+  std::vector<Employee*> staff = StaffFactory::makeStaff();
+  std::minstd_rand simple_rand;
+  simple_rand.seed(42);
+  for (auto emp : staff) {
+    emp->setWorkTime(simple_rand() % 40);
+  }
+  for (auto emp : staff) {
+    emp->calc();
+  }
+  for (auto emp : staff) {
+    emp->printInfo();
+  }
+  return 0;
 }
